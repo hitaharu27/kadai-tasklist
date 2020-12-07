02eb3a1ab71a629ea614ba71781a1af470aada9d
haruki.hitani <syoukaze227@gmail.com>
2020-12-07 12:37:05 +0000

    kadai-tasklist

--- database/migrations/2020_12_07_211046_rename_title_to_status_on_tasks__table.php
+++ database/migrations/2020_12_07_211046_rename_title_to_status_on_tasks__table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class RenameContentStatusTasks extends Migration
+class RenameTitleToStatusOnTasksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class RenameContentStatusTasks extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->renameColumn('content', 'status');
+           $table->renameColumn('title', 'status');
         });
     }
 
